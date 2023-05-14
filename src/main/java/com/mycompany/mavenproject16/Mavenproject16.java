79638b93a211aec58685700c9fc665a27c666666
@ruddy <>
2023-05-13 21:17:45 -0300

    update program1.4!

--- src/main/java/com/mycompany/mavenproject16/Mavenproject16.java
+++ src/main/java/com/mycompany/mavenproject16/Mavenproject16.java
@@ -15,66 +15,31 @@ import java.io.InputStreamReader;
 public class Mavenproject16 {
 
     public static void main(String[] args) throws IOException {
-       
-        /* Este programa permite ingresar 3 numeros e indica cual
-        es el numero mayor y cual es el numero menor*/
         
-        //con este codigo pedimos que ingrese el numero
-        System.out.println("Ingresar el primer numero"); 
-        int numero1 = cargarNumero();
+        String resultado = "";
         
-        System.out.println("Ingresar el segundo numero");   
-        int numero2 = cargarNumero();
+        int contador = 0;
+        int numero = 1;
         
-        System.out.println("Ingresar el tercer numero");     
-        int numero3 = cargarNumero();
-        
-        //con este codigo calculamos el numero mayor y el numero menor
-        calcularNumeroMayor(numero1, numero2, numero3);
-        calcularNumeroMenor(numero1, numero2, numero3);
-    }
-    private static int cargarNumero() throws IOException {
-        InputStreamReader capturarTeclado = new InputStreamReader(System.in);
-        BufferedReader buffer = new BufferedReader(capturarTeclado);
-        
-        String strNumero = buffer.readLine();
-        int numero = Integer.parseInt(strNumero);
-        
-        return numero;
-    }
-    /**
-     * Esta funcion sirve para calcular el numero mayor!
-     * @param numero1 es el primer numero ingresado!!!!!!!
-     * @param numero2
-     * @param numero3 
-     */
-    private static void calcularNumeroMayor(int numero1, int numero2, int numero3) {
-        
-        int numeroMayor = numero1;
-        
-        if(numero2 > numeroMayor) {
-            numeroMayor = numero2;
+        while(contador < 100) {
+            if(esPrimo(numero)) {
+                resultado += numero + ", ";
+                contador++;
+            }
+            numero++;
         }
-        if(numero3 > numeroMayor) {
-            numeroMayor = numero3;
-        }
-        
-        System.out.println("El numero mayor es " + numeroMayor);
-        
+          System.out.println("los primeros 100 son " + resultado);
     }
     
-    private static void calcularNumeroMenor(int numero1, int numero2, int numero3) {
+    private static boolean esPrimo(int numero) {
         
-        int numeroMenor = numero1;
-        
-        if(numero2 < numeroMenor) {
-            numeroMenor = numero2;
-        }
-        if(numero3 < numeroMenor) {
-            numeroMenor = numero3;
-        }
-        
-        System.out.println("El numero menor es " + numeroMenor);
-    }
+        boolean esUnNumeroPrimo = true;
+        for(int i = 2; i < numero; i++) {
+           if(numero % i == 0) {
+               esUnNumeroPrimo = false;
+           }
             
+        }
+        return esUnNumeroPrimo;
+    }
 }
